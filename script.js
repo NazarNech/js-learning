e6605d71298649846ad3a992d9eb2e164ef62a0f
NazarNech <nazar.nechytailo@ekreative.com>
2022-04-21 17:31:01 +0300

    add coding challenge #3

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.write('hello!');
+// coding challenge #3
+
+const numberOfComptetitions = 3;
+
+const dolphinsAverageScore = (97 + 112 + 101) / numberOfComptetitions;
+const koalasAverageScore = (109 + 95 + 123) / numberOfComptetitions;
+
+console.log(`Dolphins Score: ${Number(dolphinsAverageScore.toFixed(1))}${'\n'}Koalas Score: ${Number((koalasAverageScore.toFixed(1)))}`);
+
+if (dolphinsAverageScore > koalasAverageScore && dolphinsAverageScore >= 100)
+    console.log('Dolphins are winners');
+else if (dolphinsAverageScore < koalasAverageScore && koalasAverageScore >= 100)
+    console.log('Koalas are winners');
+else if ((dolphinsAverageScore < 100 && koalasAverageScore < 100) || dolphinsAverageScore == koalasAverageScore)
+    console.log('It`s draw! :)');
