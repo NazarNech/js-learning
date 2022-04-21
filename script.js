4f0b88ae0d741a6d7ad2ae3905abe39356c09e55
NazarNech <nazar.nechytailo@ekreative.com>
2022-04-21 17:54:29 +0300

    update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const numberOfComptetitions = 3;
 
 const dolphinsAverageScore = (97 + 112 + 101) / numberOfComptetitions;
-const koalasAverageScore = (109 + 95 + 123) / numberOfComptetitions;
+const koalasAverageScore = (109 + 95 + 106) / numberOfComptetitions;
 
 console.log(`Dolphins Score: ${Number(dolphinsAverageScore.toFixed(1))}${'\n'}Koalas Score: ${Number((koalasAverageScore.toFixed(1)))}`);
 
@@ -11,5 +11,5 @@ if (dolphinsAverageScore > koalasAverageScore && dolphinsAverageScore >= 100)
     console.log('Dolphins are winners');
 else if (dolphinsAverageScore < koalasAverageScore && koalasAverageScore >= 100)
     console.log('Koalas are winners');
-else if ((dolphinsAverageScore < 100 && koalasAverageScore < 100) || dolphinsAverageScore == koalasAverageScore)
+else if ((dolphinsAverageScore >= 100 && koalasAverageScore >= 100) && dolphinsAverageScore == koalasAverageScore)
     console.log('It`s draw! :)');
