b5ee09b17b0693e28875cccd9af986e1dc8b82e3
NazarNech <nazar.nechytailo@ekreative.com>
2022-05-10 16:15:27 +0300

    add new homework

---
+++ codingChallenge.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const arr = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}°C in ${i + 1} days`);
+  }
+}
+
+printForecast(arr);
